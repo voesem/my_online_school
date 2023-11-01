e3c0df453afa190c8b13d6cb82c68026911c8a3a
Vladimir Semikov <voesem@ya.ru>
2023-11-01 22:17:46 +0300

    Реализован CRUD для курса через ViewSets. Описан сериализатор.

--- courses/urls.py
+++ courses/urls.py
@@ -1,7 +1,13 @@
+from rest_framework.routers import DefaultRouter
+
 from courses.apps import CoursesConfig
+from courses.views import CourseViewSet
 
 app_name = CoursesConfig.name
 
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='courses')
+
 urlpatterns = [
 
-]
+] + router.urls
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from courses.models import Course
+from courses.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
