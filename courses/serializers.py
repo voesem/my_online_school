1f7cab70804fb4c9ed5e324febd264cff1b34f9c
voesem <voesem@gmail.com>
2023-11-05 15:49:49 +0300

    Для сериализатора модели курса реализовано поле вывода списка уроков

--- courses/serializers.py
+++ courses/serializers.py
@@ -3,18 +3,19 @@ from rest_framework import serializers
 from courses.models import Course, Lesson
 
 
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
+    lessons = LessonSerializer(source='lesson', many=True, read_only=True)
 
     def get_lesson_count(self, instance):
         return instance.lesson.count()
 
     class Meta:
         model = Course
-        fields = '__all__'
-
-
-class LessonSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Lesson
         fields = '__all__'
