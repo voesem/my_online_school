2fd65adca51726b232abf02a548fe11b9d676777
voesem <voesem@gmail.com>
2023-11-06 12:23:29 +0300

    Для пользователя добавлено поле payments с историей платежей

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
+from courses.serializers import PaymentListSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    payments = PaymentListSerializer(source='user', many=True, read_only=True)
+
     class Meta:
         model = User
         fields = '__all__'
