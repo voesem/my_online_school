5cac650bd2fd9f331cdbeeed74a3fe572438316a
voesem <voesem@gmail.com>
2023-11-06 12:13:01 +0300

    Добавлен контроллер для вывода списка платежей с фильтрацией и сортировкой

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, generics
+from rest_framework.filters import OrderingFilter
 
-from courses.models import Course, Lesson
-from courses.serializers import CourseSerializer, LessonSerializer
+from courses.models import Course, Lesson, Payment
+from courses.serializers import CourseSerializer, LessonSerializer, PaymentListSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -30,3 +32,12 @@ class LessonUpdateAPIView(generics.UpdateAPIView):
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentListSerializer
+    queryset = Payment.objects.all()
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('course', 'lesson', 'method',)
+    ordering_fields = ('date',)
