11e0e9dc0debf9bc2c5b33bb98e944f661255641
Vladimir Semikov <voesem@ya.ru>
2023-11-06 21:38:01 +0300

    Описаны урлы для авторизации

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import UserViewSet
@@ -11,4 +12,6 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
