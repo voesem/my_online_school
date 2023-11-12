283820a97db4af4e8a1962a844f556aa3cd9c46f
Vladimir Semikov <voesem@ya.ru>
2023-11-12 18:42:42 +0300

    Описаны права доступа для подписанных пользователей

--- courses/permissions.py
+++ courses/permissions.py
@@ -20,3 +20,12 @@ class IsNotModerator(BasePermission):
 
     def has_permission(self, request, view):
         return not request.user.groups.filter(name='Модераторы').exists()
+
+
+class IsSubscriber(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.user:
+            return True
+
+        return False
