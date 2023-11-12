e1ef3e5e9c5492fa3088ed7d54be76c32133720f
Vladimir Semikov <voesem@ya.ru>
2023-11-12 15:12:02 +0300

    Создано представление (view) для получения JWT-токена

--- users/views.py
+++ users/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, MyTokenObtainPairSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
