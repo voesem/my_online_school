c4734b9619e1215ffaf8412c90a67b6378baf0ed
Vladimir Semikov <voesem@ya.ru>
2023-11-12 15:12:51 +0300

    Добавлен эндпоинт получения токена

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserViewSet
+from users.views import UserViewSet, MyTokenObtainPairView
 
 app_name = UsersConfig.name
 
@@ -12,6 +12,6 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
