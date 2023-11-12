2bf70ac55d3b3b86c4996c8e63af3b87417b4cfa
Vladimir Semikov <voesem@ya.ru>
2023-11-12 18:42:05 +0300

    Создан сериализатор для подписки

--- courses/serializers.py
+++ courses/serializers.py
@@ -14,11 +14,18 @@ class LessonSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    subscribed = serializers.SerializerMethodField()
     lesson_count = serializers.SerializerMethodField()
     lessons = LessonSerializer(source='lesson', many=True, read_only=True)
 
     def get_lesson_count(self, instance):
         return instance.lesson.count()
+
+    def get_subscribed(self, instance):
+        request = self.context.get('request')
+        if request:
+            return Subscription.objects.filter(user=request.user, course=instance).exists()
+        return False
 
     class Meta:
         model = Course
@@ -26,7 +33,12 @@ class CourseSerializer(serializers.ModelSerializer):
 
 
 class PaymentListSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Payment
+        fields = '__all__'
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
         fields = '__all__'
